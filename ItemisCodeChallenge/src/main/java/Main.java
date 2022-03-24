00b85255753c6c4df9c5b8dd6ab95e3ceafd4ceb
inesDrebenstedt <54622994+inesDrebenstedt@users.noreply.github.com>
2022-03-24 16:33:31 +0100

    Added print out for inputs.

--- ItemisCodeChallenge/src/main/java/Main.java
+++ ItemisCodeChallenge/src/main/java/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
 	public static void main(String[] args) {	
 		
+		System.out.println("### INPUT:");
 		StringTools.printInput1();
 		StringTools.printInput2();
 		StringTools.printInput3();
 		
+		System.out.println("### OUTPUT");
 		System.out.println(ShoppingBasketForPrintingPreparer.prepareBasket1Output());
 		System.out.println(ShoppingBasketForPrintingPreparer.prepareBasket2Output());
 		System.out.println(ShoppingBasketForPrintingPreparer.prepareBasket3Output());
